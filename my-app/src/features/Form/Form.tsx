fce48040a1a881124f874d4b7142fb2341b500d7
terrence-celestine <terrencebutler89@gmail.com>
2022-07-21 09:52:00 -0700

    Setup new reducers for formSlice

--- my-app/src/features/Form/Form.tsx
+++ my-app/src/features/Form/Form.tsx
@@ -1,17 +1,36 @@
 import { useAppSelector, useAppDispatch } from "../../app/hooks";
-import { selectForm, updateName } from "./formSlice";
+import { selectForm, updateFirstName, resetName } from "./formSlice";
 export const Form = () => {
   // setup the selector
   const user = useAppSelector(selectForm);
   const dispatch = useAppDispatch();
 
+  const handleSetName = (name: string): void => {
+    dispatch(updateFirstName(name));
+  };
+
+  const handleResetName = () => {
+    dispatch(resetName());
+  };
   return (
     <div>
-      <p>This is the name value from redux:{user}</p>
-      <input
-        type="text"
-        onChange={(e) => dispatch(updateName(e.target.value))}
-      />
+      <label>
+        First Name:{" "}
+        <input
+          value={user?.firstName}
+          type="text"
+          onChange={(e) => handleSetName(e.target.value)}
+        />
+      </label>
+      <label>
+        First Name:{" "}
+        <input
+          value={user?.firstName}
+          type="text"
+          onChange={(e) => handleSetName(e.target.value)}
+        />
+      </label>
+      <button onClick={() => handleResetName()}>Reset name</button>
     </div>
   );
 };
--- my-app/src/features/Form/formSlice.ts
+++ my-app/src/features/Form/formSlice.ts
@@ -3,12 +3,14 @@ import { RootState } from "../../app/store";
 
 // Setup the interface for the Type
 export interface FormState {
-    name: string;
+    firstName: string;
+    lastName: string;
 }
 
 // Setup the initial state
 const initialState: FormState = {
-    name: ""
+    firstName: "",
+    lastName: ""
 }
 
 // Setup the slice of pie that will be used through the app
@@ -16,20 +18,21 @@ export const formSlice = createSlice({
     name: 'form',
     initialState, 
     reducers: {
-        updateName: (state, action: PayloadAction<string>) => {
-            state.name = action.payload;
+        updateFirstName: (state, action: PayloadAction<string>) => {
+            state.firstName = action.payload;
         },
         resetName: (state) => {
-            state.name = "";
+            state.firstName = "";
+            state.lastName = "";
         }
     }
 });
 
 // export the slice actions
-export const { updateName, resetName} = formSlice.actions;
+export const { updateFirstName, resetName} = formSlice.actions;
 
 // export the selector, this will be used in other files as the state value
-export const selectForm = (state:RootState) => state.form.name;
+export const selectForm = (state:RootState) => state.form;
 
 // return the reducer
 export default formSlice.reducer;
